ca78a4698094639cf2483ec9c098665e1b14430c
majd1988shahin <majd1988shahin@hotmail.com>
2019-05-11 17:58:29 +0200

    Majd Shahin RSA+AES

---
+++ openssl10_RSA_03/srn_aes.cpp
@@ -0,0 +1,201 @@
+
+#include <iostream>
+#include<stdio.h>
+#include<math.h>
+#include <fstream>
+
+#include <QCoreApplication>
+
+#include <stdlib.h>
+#include <openssl/aes.h>
+#include <openssl/evp.h>
+#include <iostream>
+#include<string.h>
+using namespace std;
+
+ class SRN_AES
+{
+private:
+
+
+     EVP_CIPHER_CTX* e_ctx = EVP_CIPHER_CTX_new();
+     EVP_CIPHER_CTX* d_ctx = EVP_CIPHER_CTX_new();
+
+
+    void p(string inputf)
+    {
+
+    }
+public:
+
+    SRN_AES(){
+
+    }
+
+    bool aes_file_enc(string filename,string pass)
+    {
+        ifstream fi( filename, ios::binary | ios::ate);
+        int size= fi.tellg();
+        fi.close();
+
+        unsigned char *buffer=(unsigned char *)alloca(size);
+        unsigned char *buffer2=(unsigned char *)alloca(200000);
+
+        //int size=0;
+        buffer=readf(&filename[0],size);
+        cout<<size<<endl;
+
+        unsigned int salt[] = {12345, 54321};
+              int key_data_len = static_cast<int>(strlen(reinterpret_cast<char *>(&pass[0])));
+              SRN_AES *aes=new SRN_AES();
+                 aes->aes_init((unsigned char*)&pass[0],key_data_len,reinterpret_cast<unsigned char *>(&salt));
+
+         string f=filename+".aes";
+
+
+         buffer2=aes->aes_encrypt(buffer,&size);
+         writef(&f[0],buffer2,size);
+         free(buffer);free(buffer2);
+         free(aes);
+         remove(&filename[0]);
+        return (size>0);
+    }
+    bool aes_file_dec(string filename,string pass)
+    {
+        ifstream fi( filename, ios::binary | ios::ate);
+        int size= fi.tellg();
+        fi.close();
+
+        unsigned char *buffer=(unsigned char *)alloca(size);
+        unsigned char *buffer2=(unsigned char *)alloca(200000);
+
+        //int size=0;
+        string f=filename+".aes";
+        buffer=readf(&f[0],size);
+        cout<<size<<endl;
+
+        unsigned int salt[] = {12345, 54321};
+              int key_data_len = static_cast<int>(strlen(reinterpret_cast<char *>(&pass[0])));
+              SRN_AES *aes=new SRN_AES();
+                 aes->aes_init((unsigned char*)&pass[0],key_data_len,reinterpret_cast<unsigned char *>(&salt));
+
+         buffer2=aes->aes_decrypt(buffer,&size);
+         writef(&filename[0],buffer2,size);
+         free(buffer);free(buffer2);
+         free(aes);
+        // remove(&filename[0]);
+        return (size>0);
+    }
+    unsigned char*  aes_file_dec_READ_ONLY(string filename,string pass,int &out_size)
+    {
+        ifstream fi( filename, ios::binary | ios::ate);
+        int size= fi.tellg();
+        fi.close();
+
+        unsigned char *buffer=(unsigned char *)alloca(size);
+        unsigned char *buffer2=(unsigned char *)alloca(200000);
+
+        //int size=0;
+        string f=filename+".aes";
+        buffer=readf(&f[0],size);
+        cout<<size<<endl;
+
+        unsigned int salt[] = {12345, 54321};
+              int key_data_len = static_cast<int>(strlen(reinterpret_cast<char *>(&pass[0])));
+              SRN_AES *aes=new SRN_AES();
+                 aes->aes_init((unsigned char*)&pass[0],key_data_len,reinterpret_cast<unsigned char *>(&salt));
+
+         buffer2=aes->aes_decrypt(buffer,&size);
+         writef(&filename[0],buffer2,size);
+         free(buffer);//free(buffer2);
+         free(aes);
+        // remove(&filename[0]);
+         out_size=size;
+        return buffer2;
+    }
+    int aes_init(unsigned char *key_data,
+                 int key_data_len, unsigned char *salt)
+    {
+        int i, nrounds = 5;
+        unsigned char key[32], iv[32];
+
+        i = EVP_BytesToKey(EVP_aes_256_cbc(), EVP_sha1(), salt, key_data, key_data_len, nrounds, key, iv);
+        if (i != 32)
+        {
+            printf("Key size is %d bits - should be 256 bits\n", i);
+            exit(-1);
+        }
+
+        EVP_CIPHER_CTX_init(e_ctx);
+        EVP_EncryptInit_ex(e_ctx, EVP_aes_256_cbc(), nullptr, key, iv);
+        EVP_CIPHER_CTX_init(d_ctx);
+        EVP_DecryptInit_ex(d_ctx, EVP_aes_256_cbc(), nullptr, key, iv);
+
+        return 0;
+    }
+
+    unsigned char *aes_encrypt( unsigned char *plaintext, int *len)
+    {
+        int c_len = *len + AES_BLOCK_SIZE, f_len = 0;
+        unsigned char *ciphertext = reinterpret_cast<unsigned char*>(malloc(static_cast<size_t>(c_len)));
+
+        EVP_EncryptInit_ex(e_ctx, nullptr, nullptr, nullptr, nullptr);
+        EVP_EncryptUpdate(e_ctx, ciphertext, &c_len, plaintext, *len);
+        EVP_EncryptFinal_ex(e_ctx, ciphertext+c_len, &f_len);
+
+        *len = c_len + f_len;
+        return ciphertext;
+    }
+
+    unsigned char *aes_decrypt(unsigned char *ciphertext, int *len)
+    {
+        int p_len = *len, f_len = 0;
+        unsigned char *plaintext = reinterpret_cast<unsigned char*>(malloc(static_cast<size_t>(p_len + AES_BLOCK_SIZE)));
+
+        EVP_DecryptInit_ex(d_ctx, nullptr, nullptr, nullptr, nullptr);
+        EVP_DecryptUpdate(d_ctx, plaintext, &p_len, ciphertext, *len);
+        EVP_DecryptFinal_ex(d_ctx, plaintext+p_len, &f_len);
+
+        *len = p_len + f_len;
+        return plaintext;
+    }
+
+
+   static unsigned char * readf(char* name,int&size )
+    {
+        ifstream f( name, ios::binary | ios::ate);
+        size= f.tellg();
+        f.close();
+
+
+         char * i = new  char[size] ;
+         fstream myFile (name );
+         myFile.read (i, size);
+         myFile.close();
+        return (unsigned char*)i;
+     }
+    char * readf2 (const char * filename, int * read)
+    {
+        ifstream ifs(filename, ios::binary|ios::ate);
+        ifstream::pos_type pos = ifs.tellg();
+        int length = pos;
+        char *pChars = new char[length];
+        ifs.seekg(0, ios::beg);
+        ifs.read(pChars, length);
+        ifs.close();
+        *read = length;
+        return pChars;
+    }
+    void writef(char* name , unsigned char * data, int size)
+    {
+        std::ofstream f;
+        f.open(name);
+        //f<<data;
+
+    f.write ((char*)data, size); f.close();
+      /*  fstream myFile (name, ios::out | ios::binary);
+        myFile.write ((char*)&data[0], sizeof (unsigned char)*size);
+        myFile.close();*/
+    }
+
+};
---
+++ openssl10_RSA_03/srn_rsa.cpp
@@ -0,0 +1,272 @@
+#include <openssl/pem.h>
+#include <openssl/ssl.h>
+#include <openssl/rsa.h>
+#include <openssl/evp.h>
+#include <openssl/bio.h>
+#include <openssl/err.h>
+#include <stdio.h>
+#include<string.h>
+#include<iostream>
+#include <stdio.h>
+
+
+#include"srn_aes.cpp"
+using namespace std;
+class SRN_RSA
+{
+
+public:
+
+    void op()
+    {
+        //RSA *rsa=createRSAWithFilename("majd_public.pem",1);
+        unsigned char  encrypted[4098]={};
+        unsigned char decrypted[4098]={};
+
+        char plainText[2048/8] = "Hello worldkhgfdsfghjkl,mnbgfvdrtyuiolkjhgfdrftyuio"; //key length : 2048
+
+        string user="majd";
+        string user2="janik";
+
+
+        string pass ="akdfvmdslvnjskjfdfghjkl;'lkjlhkjdxioio;lnkjd";
+        generate_key(user,pass);
+
+
+        //generate_key(user2,pass);
+        public_encrypt_user((unsigned char*)plainText,strlen(plainText),(unsigned char*)encrypted,user);
+
+        int decrypted_length = private_decrypt_user((unsigned char*)encrypted,256, (unsigned char*)decrypted,user,pass);
+                cout<<plainText<<endl;
+                //cout<<encrypted_length<<endl;
+                cout<<decrypted<<endl;
+       // char result[2048/8];
+/*
+//int encrypted_length= public_encrypt_user((unsigned char*)plainText,strlen(plainText),user,(unsigned char*)encrypted);
+
+*/
+    }
+
+    bool generate_key(std::string userName,string pass)
+        {
+            int             ret = 0;
+            RSA             *r = NULL;
+            BIGNUM          *bne = NULL;
+            BIO             *bp_public = NULL, *bp_private = NULL;
+
+            int             bits = 2048;
+            unsigned long   e = RSA_F4;
+
+
+            std::string pbK_Name=userName+"_public.pem";
+            std::string pK_Name=userName+"_private.pem";
+        do{
+            bne = BN_new();
+            ret = BN_set_word(bne,e);
+            if(ret != 1){
+                break;
+            }
+
+            r = RSA_new();
+            ret = RSA_generate_key_ex(r, bits, bne, NULL);
+            if(ret != 1){
+                break;
+            }
+
+            bp_public = BIO_new_file(pbK_Name.c_str(), "w+");
+            ret = PEM_write_bio_RSAPublicKey(bp_public, r);
+            if(ret != 1){
+                break;
+            }
+
+
+            bp_private = BIO_new_file(pK_Name.c_str(), "w+");
+            ret = PEM_write_bio_RSAPrivateKey(bp_private, r, NULL, NULL, 0, NULL, NULL);
+
+
+            // 4. free
+            }
+            while (0);
+
+            BIO_free_all(bp_public);
+            BIO_free_all(bp_private);
+            RSA_free(r);
+            BN_free(bne);
+
+            aes_privatek_enc(pK_Name,pass);
+
+
+            return (ret == 1);
+        }
+
+
+
+    int public_encrypt_user(unsigned char * data,int data_len, unsigned char *encrypted,string username)
+    {
+        RSA * rsa = readRSA_pub(username);
+        int result = RSA_public_encrypt(data_len,data,encrypted,rsa,padding);
+        return result;
+    }
+
+
+    int private_decrypt_user(unsigned char * enc_data,int data_len, unsigned char *decrypted,string username,string pass)
+    {
+        RSA * rsa = readRSA_pri(username,pass);
+        int  result = RSA_private_decrypt(data_len,enc_data,decrypted,rsa,padding);
+        return result;
+    }
+
+    int private_encrypt_user(unsigned char * data,int data_len, unsigned char *encrypted,string username,string pass)
+    {
+        RSA * rsa = readRSA_pri(username,pass);
+        int result = RSA_private_encrypt(data_len,data,encrypted,rsa,padding);
+        return result;
+    }
+
+    int public_decrypt_user(unsigned char * enc_data,int data_len, unsigned char *decrypted,string username)
+    {
+        RSA * rsa = readRSA_pub(username);
+        int  result = RSA_public_decrypt(data_len,enc_data,decrypted,rsa,padding);
+        return result;
+    }
+
+private:
+
+    int padding = RSA_PKCS1_PADDING;
+
+    unsigned char* aes_privatek_read(string username,string pass,int &size)
+    {
+        unsigned int salt[] = {12345, 54321};
+       int key_data_len = static_cast<int>(strlen(reinterpret_cast<char *>(&pass[0])));
+              SRN_AES *aes=new SRN_AES();
+                 aes->aes_init((unsigned char*)&pass[0],pass.length(),reinterpret_cast<unsigned char *>(&salt));
+
+         string f=username+"_private.pem";
+
+
+         //buffer2=aes->aes_decrypt(buffer,&size);
+
+         //writef(&f[0],buffer2,size);
+      //   free(buffer);//free(buffer2);
+
+         unsigned char * result =aes->aes_file_dec_READ_ONLY(f,pass,size);
+        return result ;
+    }
+    unsigned char * readf(char* name,int&size )
+    {
+        ifstream f( name, ios::binary | ios::ate);
+        size= f.tellg();
+        cout<<size<<endl;
+        f.close();
+
+        unsigned char * in = (unsigned char *) malloc(size);
+         fstream myFile (name, ios::in  | ios::binary);
+         myFile.read ((char*)in, size);
+         myFile.close();
+        return in;
+     }
+    void writef(char* name , unsigned char * data, int size)
+    {
+        std::ofstream f;
+        f.open(name);
+        //f<<data;
+
+    f.write ((char*)data, size); f.close();
+      /*  fstream myFile (name, ios::out | ios::binary);
+        myFile.write ((char*)&data[0], sizeof (unsigned char)*size);
+        myFile.close();*/
+    }
+    RSA * createRSA(unsigned char * key,int publick)
+    {
+        RSA *rsa= NULL;
+        BIO *keybio ;
+        keybio = BIO_new_mem_buf(key, -1);
+        if (keybio==NULL)
+        {
+            printf( "Failed to create key BIO");
+            return 0;
+        }
+        if(publick)
+        {
+            rsa = PEM_read_bio_RSA_PUBKEY(keybio, &rsa,NULL, NULL);
+        }
+        else
+        {
+            rsa = PEM_read_bio_RSAPrivateKey(keybio, &rsa,NULL, NULL);
+        }
+        if(rsa == NULL)
+        {
+            printf( "Failed to create RSA");
+        }
+
+        return rsa;
+    }
+
+    void printLastError(char *msg)
+    {
+        char * err =(char*) malloc(130);;
+        ERR_load_crypto_strings();
+        ERR_error_string(ERR_get_error(), err);
+        printf("%s ERROR: %s\n",msg, err);
+        free(err);
+    }
+
+    RSA * readRSA_pub(std::string username)
+    {
+        RSA *rsa=RSA_new() ;
+        FILE *f;
+
+
+            std::string filename=username+"_public.pem";
+            f = fopen(filename.c_str(), "rb");
+            rsa = PEM_read_RSAPublicKey(f, &rsa,NULL, NULL);
+
+        return rsa;
+    }
+
+    RSA * readRSA_pri(std::string username,string pass)
+    {
+     /*   RSA *rsa=RSA_new() ;
+        FILE *f;
+
+
+            std::string filename=username+"_private.pem";
+            f = fopen(filename.c_str(), "rb");
+            rsa = PEM_read_RSAPrivateKey(f, &rsa,NULL, NULL);
+
+        return rsa;*/
+
+        RSA *rsa= NULL;
+        BIO *keybio ;
+        int size=0;
+        unsigned char* key =aes_privatek_read(username,pass,size);
+        keybio = BIO_new_mem_buf(key, -1);
+        if (keybio==NULL)
+        {
+            printf( "Failed to create key BIO");
+            return 0;
+        }
+
+        rsa = PEM_read_bio_RSAPrivateKey(keybio, &rsa,NULL, NULL);
+        if(rsa == NULL)
+        {
+            printf( "Failed to create RSA");
+        }
+
+
+        return rsa;
+    }
+
+    bool aes_privatek_enc(string filename,string pass)
+    {
+        unsigned int salt[] = {12345, 54321};
+              int key_data_len = static_cast<int>(strlen(reinterpret_cast<char *>(&pass[0])));
+              SRN_AES *aes=new SRN_AES();
+                 aes->aes_init((unsigned char*)&pass[0],key_data_len,reinterpret_cast<unsigned char *>(&salt));
+
+       aes->aes_file_enc(filename,pass);
+        free(aes);
+       //  remove(&filename[0]);
+        return true;
+    }
+   };
